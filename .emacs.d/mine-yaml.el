078b60646551acb763ea43872a5e9161e1a97a6f
ikyp <ikyp.kypi@gmail.com>
2017-10-27 18:43:42 +0800

    Delete file

--- .emacs.d/mine-yaml.el
+++
@@ -1,7 +0,0 @@
-;;; pacage --- Summary
-;;; Commentary:
-;;; Code:
-(autoload 'yaml-mode "yaml-mode" "Simple mode to edit YAML" t nil)
-(add-to-list 'auto-mode-alist '("\\.yml\\'" . yaml-mode))
-(provide 'mine-yaml)
-;;; mine-yaml.el ends here
