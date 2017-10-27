0531b2989c707078d2c6d2bd4e76bd9368baab42
ikyp <ikyp.kypi@gmail.com>
2017-10-27 16:23:20 +0800

    Deleted some file.

--- .emacs.d/mine-elisp.el
+++
@@ -1,9 +0,0 @@
-;;; pacage --- Summary
-;;; Commentary:
-;;; Code:
-(autoload 'eldoc "elisp docments" t nil)
-
-(add-hook 'emacs-lisp-mode-hook '(lambda ()
-				   (local-set-key (kbd "RET") 'newline-and-indent)))
-(provide 'mine-elisp)
-;;; mine-elisp.el ends here
